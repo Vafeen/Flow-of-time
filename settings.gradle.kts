3807ec2ea929888e053d8ff42a826d316c4aca7b
Vafeen <666av6@gmail.com>
2025-05-25 19:25:17 +0300

    change project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Compose clean arch multimodule example"
+rootProject.name = "Flow of Time"
 include(":app")
 include(":presentation")
 include(":data")
